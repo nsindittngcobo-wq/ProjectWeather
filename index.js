1b3e3c880d63e08242dcaa4db11e8df3e2c60f25
nsindittngcobo-wq <nsindittngcobo@gmail.com>
2026-03-29 05:21:46 +0200

    Added axios and the api key

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let apiKey = "ec8t62ba8b244884fo89d6da5a743070";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}&units=metric";
+
+
+
+
+
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
