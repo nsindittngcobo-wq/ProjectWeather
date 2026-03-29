ed3480c9b9761ea82af963bbe0eed5ba523cb406
nsindittngcobo-wq <nsindittngcobo@gmail.com>
2026-03-29 06:05:01 +0200

    Integrated API functions onto the file

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-
-function displayTemp (response) {
-    let tempElement = document.querySelector("#weather-temp");
-    let temperature = Math.round (response.data.temperature.current);
-    tempElement.innerHTML = temperature;
-    let h1 = document.querySelector("h1")
-  h1.innerHTML = response.data.city
-    console.log(response.data.temperature);
+function displayTemp(response) {
+  let tempElement = document.querySelector("#weather-temp");
+  let temperature = Math.round(response.data.temperature.current);
+  tempElement.innerHTML = temperature;
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = response.data.city;
+  console.log(response.data.temperature);
 }
-
 
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-let city = searchInput.value;
-let apiKey = "ec8t62ba8b244884fo89d6da5a743070";
-let apiUrl =
-  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemp);
+  let city = searchInput.value;
+  let apiKey = "ec8t62ba8b244884fo89d6da5a743070";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemp);
 }
 let form = document.querySelector("#form-info");
 form.addEventListener("submit", searchCity);
